fa53208c56ab9adc028233cfd7b6e4d94d9f3c7e
csurgomartinka <150018940+csurgomartinka@users.noreply.github.com>
2026-02-09 10:38:37 +0100

    Add files via upload

---
+++ HelloVilag/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Csurgó Martin");
+        System.out.println("BN3UU8");
+        System.out.println("Minden nap egy alma az orvost távol tartja.");
+        /*
+        háztartási eszköz
+        szín: kék
+        anyag: üveg
+        folyadék tartás
+         */
+    }
+}
